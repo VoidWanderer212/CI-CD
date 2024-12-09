867cab426b58bf6c85c33f21defbd1d873deaadc
VoidWanderer212 <carmenelaine.co2@gmail.com>
2024-12-08 23:52:49 -0800

    Update notify_webex.py

--- notify_webex.py
+++ notify_webex.py
@@ -2,8 +2,8 @@ import requests
 import os
 
 # Get the WebEx token and room ID from environment variables
-WEBEX_TOKEN = os.getenv("ZGM2ZTJjOGItYWQxOS00ODY3LWE4OGItYWFiMzBjZjZmYzcwMGRlOTBkYjAtZTFl_P0A1_18df2bf4-66a3-4793-b661-314dcbe95852")  # Replace with your actual environment variable name
-ROOM_ID = os.getenv("a58cfa40-b5e6-11ef-bf09-6910fd7587cf")          # Replace with your actual environment variable name
+WEBEX_TOKEN = ("ZGM2ZTJjOGItYWQxOS00ODY3LWE4OGItYWFiMzBjZjZmYzcwMGRlOTBkYjAtZTFl_P0A1_18df2bf4-66a3-4793-b661-314dcbe95852")  # Replace with your actual environment variable name
+ROOM_ID = ("a58cfa40-b5e6-11ef-bf09-6910fd7587cf")          # Replace with your actual environment variable name
 
 def send_webex_notification(message):
     """
